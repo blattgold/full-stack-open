e627a3b2a285065945111acf41f0fac8882a99d4
Ashli <blattgoldowo@zohomail.eu>
2023-09-13 13:14:32 +0200

    completed 2c

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const DisplayPerson = ({person}) => <>{person.name} {person.number}</>
 
@@ -44,15 +45,18 @@ const PersonForm = ({onSubmit, nameOnChange, nameValue, numberOnChange, numberVa
 }
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-1234567', id: 0 },
-		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
-		{ name: 'Dan Abramov', number: '12-43-234345', id: 2 },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 3}
-	])
+	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
 	const [searchFilter, setSearchFilter] = useState('')
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:3001/persons')
+			.then(response => {
+				setPersons(response.data)
+			})
+	}, [])
 
 	const addPerson = (event) => {
 		event.preventDefault()
