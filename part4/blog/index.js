0eaeaca97a833e06866c024317560735cde30b30
Ashli <blattgoldowo@zohomail.eu>
2023-11-03 13:25:43 +0100

    done 4.2, refactored into modules

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,19 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 
-const Blog = require('./models/blog')
 const logger = require('./utils/logger')
 const config = require('./utils/config') 
-const blogsRouter = require('./controllers/blogs')
-
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
 	logger.info(`Server running on port ${config.PORT}`)
