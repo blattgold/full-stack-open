8ef74c7112e306138ede46894ab307fdba24d82a
Ashli <blattgoldowo@zohomail.eu>
2023-11-12 17:44:58 +0100

    4.18 done

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -8,6 +8,7 @@ const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
@@ -21,6 +22,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
